df444a0ec37e7798a3ff39f2660fbe6082321041
jeeewon <jiwon9586@gmail.com>
2022-05-15 16:11:40 +0900

    HelloController

--- src/main/java/org/example/demo/web/HelloController.java
+++ src/main/java/org/example/demo/web/HelloController.java
@@ -1,6 +1,8 @@
 package org.example.demo.web;
 
 import org.example.demo.web.dto.HelloResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,6 +16,24 @@ public class HelloController {
     public HelloResponseDto helloDto(@RequestParam("name") String name,
                                      @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
+    }
+    private static final Logger logger = LoggerFactory.getLogger(HelloController .class);
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(
+            @RequestHeader(value="Accept") String accept,
+            @RequestHeader(value="Accept-Language") String acceptLanguage,
+            @RequestHeader(value="User-Agent", defaultValue="myBrowser") String userAgent,
+            @RequestHeader(value="Host") String host
+    ) {
+
+        System.out.println("Accept: " + accept);
+        System.out.println("Accept-Language: " + acceptLanguage);
+        System.out.println("User-Agent: " + userAgent);
+        System.out.println("Host: " + host);
+
+        return "home";
     }
 }
 /*
