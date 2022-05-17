5102837730236db6a82af18c9b4a5dc890301674
lmj <70003894+llprn@users.noreply.github.com>
2022-05-17 13:45:00 +0900

    Update WebConfig.java

--- src/main/java/org/example/demo/config/WebConfig.java
+++ src/main/java/org/example/demo/config/WebConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
+/*    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
                 .allowedOrigins("http://localhost:8000")
         ;
-    }
+    }*/
 }
