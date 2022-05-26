2de63e8a375ee2e647d50dfb5e763770bc70b778
lmj <70003894+llprn@users.noreply.github.com>
2022-05-27 00:47:04 +0900

    Update Application.java
    add import

--- src/main/java/org/example/demo/Application.java
+++ src/main/java/org/example/demo/Application.java
@@ -3,6 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 //@EnableJpaAuditing
 
