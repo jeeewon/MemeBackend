3ae175f0f4a66c1417a991a89f978cb793e6d3c5
jeeewon <jiwon9586@gmail.com>
2022-06-03 16:49:43 +0900

    change password

---
+++ src/main/java/org/example/demo/web/dto/member/ChangePwdDto.java
@@ -0,0 +1,16 @@
+package org.example.demo.web.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePwdDto {
+
+    private String oldPassword;
+    private String newPassword;
+}
