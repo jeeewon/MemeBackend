67f37822ae1cef656e6baa82af54ac9c5975df05
lmj <70003894+llprn@users.noreply.github.com>
2022-06-10 01:20:25 +0900

    del properties file path

--- src/main/java/org/example/demo/Application.java
+++ src/main/java/org/example/demo/Application.java
@@ -9,13 +9,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:aws.properties";
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
