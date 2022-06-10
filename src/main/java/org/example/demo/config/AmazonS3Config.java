b51b8022ce0c75d0eb89fe3fe1a616a34d9dc04e
lmj <70003894+llprn@users.noreply.github.com>
2022-06-10 12:10:31 +0900

    Update AmazonS3Config.java

--- src/main/java/org/example/demo/config/AmazonS3Config.java
+++ src/main/java/org/example/demo/config/AmazonS3Config.java
@@ -23,7 +23,7 @@ public class AmazonS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client(){
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey,secretKey);
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(this.accessKey,this.secretKey);
 
         return(AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
