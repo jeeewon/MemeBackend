7e26cd55cf023b3a16e0becc409bd3fe10bbae9a
lmj <70003894+llprn@users.noreply.github.com>
2022-06-10 12:21:26 +0900

    Update Application.java

--- src/main/java/org/example/demo/Application.java
+++ src/main/java/org/example/demo/Application.java
@@ -9,8 +9,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
+     public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.properties";
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
-
 }
