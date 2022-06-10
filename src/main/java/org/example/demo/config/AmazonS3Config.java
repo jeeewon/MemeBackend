c51a12c288e4a7787514f3e8750d92d590d676ca
lmj <70003894+llprn@users.noreply.github.com>
2022-06-10 12:19:11 +0900

    Update AmazonS3Config.java

--- src/main/java/org/example/demo/config/AmazonS3Config.java
+++ src/main/java/org/example/demo/config/AmazonS3Config.java
@@ -14,10 +14,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonS3Config {
-/*    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
     @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;*/
+    private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
 
