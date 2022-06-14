66b3e8846ff73251ce4fc4a24976b831371ed971
lmj <70003894+llprn@users.noreply.github.com>
2022-06-14 21:38:31 +0900

    delete comments

--- src/main/java/org/example/demo/web/dto/member/MyPagePostDto.java
+++ src/main/java/org/example/demo/web/dto/member/MyPagePostDto.java
@@ -7,13 +7,11 @@ import lombok.ToString;
 @ToString
 public class MyPagePostDto {
     Integer id;
-    //Integer file_id;
     String image;
     String title;
     String description;
     public MyPagePostDto(Integer id, String image,String title,String description) {
         this.id = id;
-        //this.file_id = file_id;
         this.image = image;
         this.title = title;
         this.description = description;
